c39465bc4d0fb921c843d7e72e0c1da3256257f1
Sanjose_N <125823013+Dex-21@users.noreply.github.com>
2024-02-25 14:56:43 +0530

    place and place 2 are added

---
+++ place.py
@@ -0,0 +1,5 @@
+li=[]
+for i in range(10):
+    a=i*5+23
+    li.append(a)
+print(li)
---
+++ place2.py
@@ -0,0 +1,7 @@
+li=[0,2,8]
+for i in range(10):
+    a=i*536
+    li.append(a)
+for i in range(13):
+    print(li[i]*2)
+print("Problem Resolved")
